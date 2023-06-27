7cb9cefe10a8bf4a658a12b363c17f0c9aa65b3b
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2023-06-27 21:59:06 +0900

    2つのテキストファイルを単一のCSVファイルに出力する。

---
+++ gzen.ps1
@@ -0,0 +1,17 @@
+﻿Set-StrictMode -Version latest
+
+Set-Variable -name DELIMITER -value ',' -option Constant 
+
+
+try {
+  $header = Get-Content -Path .\header.txt -Encoding UTF8 -ErrorAction Stop
+  $values = Get-Content -Path .\values.txt -Encoding UTF8 -ErrorAction Stop
+
+  $csv_object = $values | ConvertFrom-Csv -Header $header
+
+  $csv_object | Export-Csv -NoTypeInformation -Path .\exported.csv -Delimiter $DELIMITER -Encoding UTF8
+}
+catch {
+  Write-Host "エラーが発生しました：$($_.Exception.Message)"
+}
+
