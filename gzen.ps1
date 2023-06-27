5e3163e89db0f7b32a4b7e97d24ea8a5b7303b97
mdbmkz <120311179+madobemikazuki@users.noreply.github.com>
2023-06-27 21:33:20 +0900

    2つのテキストファイルを読み込んでCSVファイルとして出力する
    ヘッダーファイルとバリューファイルを単一のCSVファイルに出力する。

---
+++ gzen.ps1
@@ -0,0 +1,11 @@
+﻿Set-StrictMode -Version latest
+
+Set-Variable -name DELIMITER -value ',' -option Constant 
+
+$header = Get-Content -Path .\header.txt -Encoding UTF8
+$values = Get-Content -Path .\values.txt -Encoding UTF8
+
+$csv_object = $values | ConvertFrom-Csv -Header $header
+
+$csv_object | Export-Csv -NoTypeInformation -Path .\exported.csv -Delimiter $DELIMITER -Encoding UTF8
+
